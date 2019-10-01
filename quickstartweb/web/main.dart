221de880ebdebe17d57537ab37fa705a480a0445
Murat ÖNER <info@muratoner.net>
2019-10-01 13:58:31 +0300

    Has some changes for dart web project.

--- quickstartweb/web/main.dart
+++ quickstartweb/web/main.dart
@@ -1,5 +1,19 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  // querySelector('#output').text = 'Your Dart app is running.';
+
+  Element output = querySelector('#output');
+  output.children.addAll(thingsTodo().map(newLI));
 }
+
+LIElement newLI(String itemText) => LIElement()..text = itemText;
+
+List<String> thingsTodo() { 
+  return [
+    "Walk the dogs",
+    "Wash the dogs",
+    "Feed the cats",
+    "Feed the dogs"
+  ];
+ }
